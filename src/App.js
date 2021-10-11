2eede13b6a1315cdd2095d965c1415917e6d580d
Aldi Pranata <aldyjrz@gmail.com>
2021-10-11 14:11:06 +0700

    belajar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./components/Header";
+
+import { Button } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+     <div className="App"> 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input  className="form-control mt-3 w-50"  type="text" />
+   <Button variant="success mt-3">Tambah</Button>
+  
         <a
           className="App-link"
           href="https://reactjs.org"
